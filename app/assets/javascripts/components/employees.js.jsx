45d13d90798cd68b8b2807e39063a5aa151e81d0
Richard LaFranchi <rlafranchi@icloud.com>
2016-04-09 17:26:39 -0600

    hiring employees

--- app/assets/javascripts/components/employees.js.jsx
+++ app/assets/javascripts/components/employees.js.jsx
@@ -1,6 +1,54 @@
 var Employees = React.createClass({
   getInitialState() {
-    return { employees: this.props.employees }
+    return {
+      employees: this.props.employees,
+      employee: {
+        name: '',
+        email: '',
+        manager: false
+      },
+      errors: {}
+    }
+  },
+
+  handleNameChange(e) {
+    var newEmployee = this.state.employee
+    newEmployee.name = e.target.value
+    this.setState({employee: newEmployee});
+  },
+
+  handleEmailChange(e) {
+    var newEmployee = this.state.employee
+    newEmployee.email = e.target.value
+    this.setState({employee: newEmployee});
+  },
+
+  handleManagerChange(e) {
+    var newEmployee = this.state.employee
+    newEmployee.manager = e.target.value
+    this.setState({employee: newEmployee});
+  },
+
+  handleHireEmployee() {
+    var that = this;
+    $.ajax({
+      method: 'POST',
+      data: {
+        employee: that.state.employee,
+      },
+      url: '/employees.json',
+      success: function(res) {
+        var newEmployeeList = that.state.employees;
+        newEmployeeList.push(res);
+        that.setState({
+          employees: newEmployeeList,
+          errors: {}
+        });
+      },
+      error: function(res) {
+        that.setState({errors: res.responseJSON.errors})
+      }
+    });
   },
 
   render() {
@@ -23,6 +71,18 @@ var Employees = React.createClass({
             </thead>
             <tbody>
               {employees}
+              <tr>
+                <td>
+                  <input type="text" onChange={this.handleNameChange} /><br />
+                  <span style={{color: 'red'}}>{this.state.errors.name}</span>
+                </td>
+                <td>
+                  <input type="text" onChange={this.handleEmailChange} /><br />
+                  <span style={{color: 'red'}}>{this.state.errors.email}</span>
+                </td>
+                <td><input type="checkbox" onChange={this.handleManagerChange} /></td>
+                <td><button onClick={this.handleHireEmployee}>Hire</button></td>
+              </tr>
             </tbody>
           </table>
         </div>

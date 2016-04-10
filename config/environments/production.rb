534cfc0e814ee607c7022604dfd54e7bc4d53659
Richard LaFranchi <rlafranchi@icloud.com>
2016-04-09 19:07:14 -0600

    add server rendering

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.react.variant = :production
+  config.react.server_renderer_options = {
+    files: ["components_server.js", "components.js"], # files to load for prerendering
+  }
 end
